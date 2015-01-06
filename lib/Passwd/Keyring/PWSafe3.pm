7b9f80845ac60c5ff2e297967b070fa1f33324c7
Marcin Kasperski <Marcin.Kasperski@address.censored.com>
2015-01-06 19:57:07 +0000

    Import of MEKK/Passwd-Keyring-PWSafe3-0.2004.tar.gz from CPAN.
    gitpan-cpan-distribution: Passwd-Keyring-PWSafe3
    gitpan-cpan-version:      0.2004
    gitpan-cpan-path:         MEKK/Passwd-Keyring-PWSafe3-0.2004.tar.gz
    gitpan-cpan-author:       MEKK
    gitpan-cpan-maturity:     released

--- lib/Passwd/Keyring/PWSafe3.pm
+++ lib/Passwd/Keyring/PWSafe3.pm
@@ -15,11 +15,11 @@ Passwd::Keyring::PWSafe3 - Password storage based on Password Safe encrypted fil
 
 =head1 VERSION
 
-Version 0.2003
+Version 0.2004
 
 =cut
 
-our $VERSION = '0.2003';
+our $VERSION = '0.2004';
 
 our $APP_NAME = "Passwd::Keyring";
 our $FOLDER_NAME = "Perl-Passwd-Keyring";
